f5905dd81036271afa9d0e714e96d7e09a164970
Toni Pano <toni.pano19@gmail.com>
2021-12-31 01:44:10 -0500

    added comment to laptop_comms.h about start dat

--- software/Groundy/ground_station_control_test/laptop_comms.h
+++ software/Groundy/ground_station_control_test/laptop_comms.h
@@ -1,4 +1,7 @@
-//laptop commands
+/*defines laptop commands for laptop to send to ground station.
+
+Started by Toni Pano on Dec 22, 2021*/
+
 #define PASS_DATA_CMD 'P'   //pass data in ground station data buffer to laptop
 #define STORE_DATA_CMD 'S'  //do not pass data to laptop, store incoming data in ground station data buffer
 
